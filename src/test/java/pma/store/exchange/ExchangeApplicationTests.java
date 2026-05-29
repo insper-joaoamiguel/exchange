b1c51373ba40361530594ea10f759e21b6d020c6
jota <joaopedroamiguel@gmail.com>
2026-05-28 21:24:03 -0300

    Setup inicial do exchange

---
+++ src/test/java/pma/store/exchange/ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package pma.store.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
